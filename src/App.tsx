8e6c00ddd650c90a049f857ec6ebae493e2290fa
sebastian-sonne <sebastian@familie-sonne.com>
2024-05-02 17:49:32 -0500

    setup compl

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={''} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={''} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center items-center w-screen h-screen'>
+        <button className='px-4 py-2 m-4 border border-blue-500 hover:bg-blue-500 hover:text-white font-semibold rounded-lg transition-color' onClick={() => setCount(c => c + 1)}>Count: {count}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
