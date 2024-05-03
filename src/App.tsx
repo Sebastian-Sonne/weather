6cd3e036b1e8b74b5360d910359cd9d263a41e3c
sebastian-sonne <sebastian@familie-sonne.com>
2024-05-02 20:40:48 -0500

    main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react'
 
-function App() {
+function App(): JSX.Element {
 
-  const [count, setCount] = useState(0)
-
+  //initial load
   var useTheme = false;
   if ('theme' in localStorage) {
     useTheme = localStorage.theme === 'dark';
@@ -11,7 +10,7 @@ function App() {
     useTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
     localStorage.theme = useTheme ? 'dark' : 'light';
   }
-  const [dark, setDark] = useState(useTheme);
+  const [dark, setDark] = useState<boolean>(useTheme);
 
   const toggleTheme = () => {
     setDark(!dark);
@@ -19,23 +18,13 @@ function App() {
   }
 
   useEffect(() => {
-    (dark) ? document.body.classList.add("dark")
+    (dark) ? document.body.classList.add('dark')
       : document.body.classList.remove('dark');
   }, [dark]);
 
-  const handleClick = () => {
-    setCount(c => c + 1);
-    toggleTheme();
-  }
-
   return (
     <>
-      <div className='flex justify-center items-center w-screen h-screen dark:bg-gray-800'>
-        <button 
-          className='px-4 py-2 m-4 border dark:text-white border-blue-500 hover:bg-blue-500 hover:text-white active:bg-blue-800 active:border-blue-800 font-semibold rounded-lg transition-all' 
-          onClick={handleClick}>Count: {count}
-        </button>
-      </div>
+      
     </>
   )
 }
